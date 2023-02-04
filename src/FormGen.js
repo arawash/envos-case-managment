ba76ff75e48c07a246613912291eebc28c470b6d
arawash <rawash.amr@gmail.com>
2023-02-05 01:04:22 +0200

    Update FormGen.js

--- src/FormGen.js
+++ src/FormGen.js
@@ -3,15 +3,7 @@ function FormGen() {
 
     return (
         <>
-        <script>
-const node = document.createElement("li");
-const textnode = document.createTextNode("Water");
-node.appendChild(textnode);
-
-document.body.appendChild(node).innerHTML = "Hello JavaScript!";
-
-
-        </script> 
+        Form
         </>
     );
 }
